fa078cf14a8436eb246f2a763571f8e9af4cd5a5
tnwjd2050 <tnwjd2050@naver.com>
2021-01-26 21:27:39 +0900

    임시 data 추가

--- js/script.js
+++ js/script.js
@@ -2,10 +2,22 @@ var smPortfolio = (function (d, w, $) {
   var smPortfolio = smPortfolio || {};
 
   var test = function() {
-    console.log('test')
+    
+  };
+
+  var getJSONInit = function (data) {
+    var html = '';
+    $.each(data.list, function (itemIndex, item) {
+      html += '<li>';
+      html += '<p>' + item.name + '</p>';
+      html += '</li>';
+    });
+
+    $('ul').html(html);
   };
 
   smPortfolio.init = function () {
+    $.getJSON('../json/data.json', getJSONInit);
     test();
   };
 

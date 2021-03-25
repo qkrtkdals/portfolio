9b43a07b1c24bcbf55c3dc77537f22e439130ddb
tnwjd2050 <tnwjd2050@naver.com>
2021-03-25 22:37:56 +0900

    detail 페이지 추가 및 그 외 페이지 변경

--- js/script.js
+++ js/script.js
@@ -1,23 +1,54 @@
 var smPortfolio = (function (d, w, $) {
   var smPortfolio = smPortfolio || {};
 
-  var test = function() {
-    
+  var getParam = function(paramName) {
+    var params = location.search.substr(location.search.indexOf("?") + 1);
+    var paramValue = "";
+
+    params = params.split("&");
+
+    for (var i = 0; i < params.length; i++) {
+        temp = params[i].split("=");
+        if ([temp[0]] == paramName) { paramValue = temp[1]; }
+    }
+
+    return paramValue;
   };
 
-  var getJSONInit = function (data) {
+  var getJSONHome = function (data) {
     var html = '';
-    $.each(data.list, function (itemIndex, item) {
+    $.each(data, function (itemIndex, item) {
       html += '<li class="thumbItem">';
-      html += '    <img src="" alt="">';
-      html += '    <div class="thumbCover">';
-      html += '        <p class="thumbCoverTitle">' + item.title + '</p>';
-      html += '        <p class="thumbCoverDate">' + item.date + '</p>';
-      html += '    </div>';
+      html += '    <a href="./detail.html?detail=' + item.id + '">';
+      html += '        <img src="" alt="">';
+      html += '            <div class="thumbCover">';
+      html += '                <p class="thumbCoverTitle">' + item.title + '<br>- ' + item.subTitle + '</p>';
+      html += '                <p class="thumbCoverDate">' + item.date + '</p>';
+      html += '        </div>';
+      html += '    </a>';
       html += '</li>';
     });
 
     $('.thumb').html(html);
+  };
+
+  var getJSONDetail = function (data) {
+    var obj = data.filter(function(item) {
+      return item.id === getParam('detail');
+    });
+
+    var motionHTML =
+      '<li>Director - ' + obj[0].visualDetail.motion.director + '</li>' +
+      '<li>Design - ' + obj[0].visualDetail.motion.design + '</li>' +
+      '<li>Animation - ' + obj[0].visualDetail.motion.animation + '</li>' +
+      '<li>Character Design & Compositing - ' + obj[0].visualDetail.motion.design2 + '</li>' +
+      '<li>Sound - ' + obj[0].visualDetail.motion.sound + '</li>';
+
+    $('.visualTitle').html('<p>' + obj[0].title + '</p><span>' + obj[0].subTitle + '</span>');
+    $('.visualDetail .client p').text(obj[0].visualDetail.client);
+    $('.visualDetail .production p').text(obj[0].visualDetail.production);
+    $('.visualDetail .motion ul').html(motionHTML);
+    $('.visualDetail .column:last-child').html(obj[0].visualDetail.text);
   };
 
   smPortfolio.init = function () {
@@ -27,13 +58,15 @@ var smPortfolio = (function (d, w, $) {
     // 운영용
     var jsonSrc = '//raw.githubusercontent.com/qkrtkdals/portfolio/master/json/data.json';
 
-    test();
     includeHTML();
 
     if ($('#home').length) {
-      $.getJSON(jsonSrc, getJSONInit);
+      $.getJSON(jsonSrc, getJSONHome);
     }
 
+    if ($('#detail').length) {
+      $.getJSON(jsonSrc, getJSONDetail);
+    }
   };
 
   return smPortfolio;
